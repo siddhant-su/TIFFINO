98c8aac9ac5accbb7be5cd49f30a919163b18eed
Sathveeka <114345430+Sathveeka@users.noreply.github.com>
2025-12-11 19:10:44 +0530

    ReviewCommentController.java

--- Backend/tiffino_admin_review/src/main/java/com/example/tiffino_admin_review/controller/ReviewCommentController.java
+++ Backend/tiffino_admin_review/src/main/java/com/example/tiffino_admin_review/controller/ReviewCommentController.java
@@ -11,7 +11,7 @@ import java.util.List;
 import java.util.Map;
 @CrossOrigin("*")
 @RestController
-@RequestMapping("/rev")
+@RequestMapping("/api/rev")
 public class ReviewCommentController {
 
     @Autowired
